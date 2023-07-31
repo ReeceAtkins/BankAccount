33555e1a43a1dac0f5beebf56c98995a2c9078b3
Reece Atkins <Reecepowered2@hotmail.com>
2023-07-31 15:22:05 -0700

    Added Deposit Unit Tests

---
+++ BankAccountTests/AccountTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount.Tests
+{
+    [TestClass()]
+    public class AccountTests
+    {
+        [TestMethod()]
+        public void Deposit_APositiveAmount_AddToBalance()
+        {
+            Account acc = new("J Doe");
+            acc.Deposit(100);
+
+            Assert.AreEqual(100, acc.Balance);
+        }
+
+        [TestMethod]
+        public void Deposit_APostitiveAmount_ReturnsUpdatedBalance()
+        {
+            // AAA - Arrange Act Assert
+            // Arrange
+            Account account = new("J. Doe");
+            double depositAmount = 100;
+            double expectedReturn = 100;
+
+            // Act
+            double returnValue = account.Deposit(depositAmount);
+
+            // Assert
+            Assert.AreEqual(expectedReturn, returnValue);
+        }
+
+    }
+}
